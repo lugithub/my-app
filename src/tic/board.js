40ef2d8c44bb969dca95a6641ae78855180f1bef
lubin <lu.bin.lu.bin@gmail.com>
2018-12-14 12:01:29 -0500

    tic 3rd commit

--- src/tic/board.js
+++ src/tic/board.js
@@ -1,54 +1,21 @@
 import React from 'react';
 
 import Square from './square';
-import calculateWinner from './calculate-winner';
 
 export default class Board extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    }
   }
   
   renderSquare(i) {
     return <Square
-      value={this.state.squares[i]}
-      onClick={this.handleClick.bind(this, i)} />;
-  }
-
-  handleClick(i) {
-    if (this.winner || this.state.squares[i]) {
-      return;
-    }
-
-    this.setState(({ squares, xIsNext }, props) => {
-      const squaresNew = squares.map((item, index) => index === i ? this.nextLabel : item);
-      return {
-        squares: squaresNew,
-        xIsNext: !xIsNext,
-      };
-    });
-  }
-
-  get winner() {
-    return calculateWinner(this.state.squares);
-  }
-
-  get nextLabel() {
-      return this.state.xIsNext ? 'X' : 'O';
+      value={this.props.squares[i]}
+      onClick={this.props.onClick.bind(null, i)} />;
   }
 
   render() {
-    let status = `Next player: ${this.nextLabel}`;
-    if (this.winner) {
-      status = `Winner: ${this.winner}`;
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
--- src/tic/game.js
+++ src/tic/game.js
@@ -2,17 +2,84 @@ import React from 'react';
 
 import Board from './board';
 import './tic.css';
+import calculateWinner from './calculate-winner';
 
 export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
+  
+  handleClick(i) {
+
+    if (this.winner || this.squares[i]) {
+      return;
+    }
+
+    this.setState(({ history, xIsNext }, props) => {
+      const squaresNew = this.squares.map((item, index) => index === i ? this.nextLabel : item);
+      return {
+        history: [...history, { squares: squaresNew }],
+        xIsNext: !xIsNext,
+      };
+    });
+  }
+
+  get winner() {
+    return calculateWinner(this.squares);
+  }
+
+  get nextLabel() {
+      return this.state.xIsNext ? 'X' : 'O';
+  }
+
+  get squares() {
+    const { history } = this.state;
+    const i = history.length - 1;
+    const { squares } = history[i];
+    return squares;
+  }
+
+  get status() {
+    let status = `Next player: ${this.nextLabel}`;
+    if (this.winner) {
+      status = `Winner: ${this.winner}`;
+    }
+    return status;
+  }
+
+  jumpTo(i) {
+    const history = this.state.history.slice(0, i + 1);
+    this.setState((state, props) => {
+      return {
+        history,
+      };
+    });
+  }
+
   render() {
+    let moves = this.state.history.map(
+      (item, index) => (
+        <li key={index}>
+          <button onClick={this.jumpTo.bind(this, index)}>{index === 0 ? 'Goto start' : `Goto step ${index}`}</button>
+        </li>));
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={this.squares}
+            status={this.status}
+            onClick={this.handleClick.bind(this)} />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{this.status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
